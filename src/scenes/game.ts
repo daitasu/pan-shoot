31bea4add3226f6837eddefc500e8dc1bb1ddca2
daitasu <ifctp.dai.monara@gmail.com>
2022-08-28 17:19:10 +0900

    feat: set base map of game

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -1,16 +1,41 @@
 ﻿import * as Phaser from "phaser";
 
 export class Game extends Phaser.Scene {
+  private map?: Phaser.Tilemaps.Tilemap
+  private tiles?: Phaser.Tilemaps.Tileset
+  private map_ground_layer?: any
+
+  private map_ground: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+  ] // 20 * 15
+
   init() {
     console.log("Game Main");
   }
 
   preload() {
-    console.log('Load assets.')
+    this.load.image('mapTiles', `../../public/images/map_tile.png`)
   }
 
   create() {
-    console.log('create.')
+    // map の読み込み
+    this.map = this.make.tilemap({ data: this.map_ground, tileWidth: 40, tileHeight: 40 })
+    this.tiles = this.map.addTilesetImage('mapTiles')
+    this.map_ground_layer = this.map.createLayer(0, this.tiles, 0, 0)
   }
 
   update() {

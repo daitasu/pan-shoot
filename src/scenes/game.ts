99066029ea444e9d6f35f0272834c61099c8cac9
daitasu <ifctp.dai.monara@gmail.com>
2022-08-28 17:23:47 +0900

    feat: set player image

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -4,6 +4,7 @@ export class Game extends Phaser.Scene {
   private map?: Phaser.Tilemaps.Tilemap
   private tiles?: Phaser.Tilemaps.Tileset
   private map_ground_layer?: any
+  private hero?: Phaser.GameObjects.Sprite
 
   private map_ground: number[][] = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -29,6 +30,7 @@ export class Game extends Phaser.Scene {
 
   preload() {
     this.load.image('mapTiles', `../../public/images/map_tile.png`)
+    this.load.spritesheet('hero', '../../public/images/hero.png', { frameWidth: 32, frameHeight: 32 });
   }
 
   create() {
@@ -36,6 +38,9 @@ export class Game extends Phaser.Scene {
     this.map = this.make.tilemap({ data: this.map_ground, tileWidth: 40, tileHeight: 40 })
     this.tiles = this.map.addTilesetImage('mapTiles')
     this.map_ground_layer = this.map.createLayer(0, this.tiles, 0, 0)
+
+    this.hero = this.add.sprite(420, 300, 'hero', 10)
+    this.hero.setDisplaySize(40, 40)
   }
 
   update() {

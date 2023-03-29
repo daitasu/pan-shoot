06c50511d514768fffbb1839f4e97c466006d73f
daitasu <ifctp.dai.monara@gmail.com>
2023-03-30 01:59:43 +0900

    feat: Set profile name

--- frontend/src/entities/text/UserRecordText.ts
+++ frontend/src/entities/text/UserRecordText.ts
@@ -1,19 +1,25 @@
 import PlainText from "./PlainText";
 
 export default class UserRecordText extends PlainText {
-  constructor(scene: Phaser.Scene) {
+  constructor(scene: Phaser.Scene, username: string) {
     const score = sessionStorage.getItem("score");
     const displayScore =
       !!score && parseInt(score, 10) ? parseInt(score, 10) : "-";
 
-    super(scene, { x: 500, y: 250 }, `今回の戦績: ${displayScore}`, {
-      style: {
-        fontSize: "30px",
-        padding: {
-          x: 0,
-          y: 32,
+    super(
+      scene,
+      { x: 500, y: 250 },
+      `${username}\n\n今回の戦績: ${displayScore}`,
+      {
+        style: {
+          fontSize: "30px",
+          padding: {
+            x: 0,
+            y: 32,
+          },
+          align: "left",
         },
-      },
-    });
+      }
+    );
   }
 }
--- frontend/src/scenes/mypage.ts
+++ frontend/src/scenes/mypage.ts
@@ -3,13 +3,20 @@ import { BK_COLOR } from "../constants";
 import ReturnTitleText from "../entities/text/RetrunTitleText";
 import UserRecordText from "../entities/text/UserRecordText";
 import axios from "axios";
+import PlainText from "../entities/text/PlainText";
 
 export class Mypage extends Phaser.Scene {
+  private profileImage: string;
+  private profileName: string;
+
   async init() {
     try {
       // 既にログイン済みなら情報を流し込む
-      const result = await axios.get("/api/me");
-      console.log("result ->", result);
+      const { data } = await axios.get("/api/me");
+      if (data) {
+        this.profileImage = data.picture;
+        this.profileName = data.name;
+      }
     } catch (err) {
       if (err.response.status === 401) {
         // Google ログインする
@@ -23,7 +30,29 @@ export class Mypage extends Phaser.Scene {
   create() {
     this.cameras.main.setBackgroundColor(BK_COLOR);
 
-    new UserRecordText(this);
+    // Ranking Title
+    new PlainText(this, { x: 150, y: 50 }, "RANKING TOP 10", {
+      style: {
+        color: "#ffffff",
+        fontSize: "30px",
+      },
+    });
+    // Ranking User
+    new PlainText(
+      this,
+      { x: 60, y: 330 },
+      "1. -\n\n2. -\n\n3. -\n\n4. -\n\n5. -\n\n6. -\n\n7. -\n\n8. -\n\n9. -\n\n10. -\n\n",
+      {
+        style: {
+          color: "#ffffff",
+          fontSize: "20px",
+        },
+      }
+    );
+
+    setTimeout(() => {
+      new UserRecordText(this, this.profileName);
+    }, 1000);
     new ReturnTitleText(this);
   }
 }
